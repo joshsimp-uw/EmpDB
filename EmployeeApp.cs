4cf610a6df9813b4f101e6c26961e988f0b3a516
Joshua Simpson <163743276+MrJSimpson@users.noreply.github.com>
2025-03-12 12:56:57 -0700

    Added old "DbApp.cs" to refer to for developing the new "EmployeeApp.cs". This way most of the boiler-plate code is there with only having to

---
+++ EmployeeApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpDB
+{
+  internal class EmployeeApp
+  {
+    private List<IPayable> payments = new List<IPayable>();
+
+  }
+}
